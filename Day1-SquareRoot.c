62bc78791b93b689efc1bc6f00e617adbe49c035
Himanshu Aggarwal <67819808+Himanshu-Agg12@users.noreply.github.com>
2021-07-18 01:52:11 +0530

    Update Day1-SquareRoot.c

--- Day1-SquareRoot.c
+++ Day1-SquareRoot.c
@@ -1,37 +1,44 @@
-#include<stdio.h>
-#include <math.h>
-float square_root(int n, int p) {
+/*
+ * Program to print square root of a number upto given precission without using sqrt operator
+ *
+ * @Himanshu Aggarwal    id-> 1911981022
+ * 17/7/2021
+ * Assignment -> 1
+ * (Resource - gfg)
+ */
+
+#include <stdio.h>
+
+float square_root (int n, int p) {
         float ans;
         int s = 0, e = n;
-        while(s<=e){
-                int mid = (e+s)/2;
-                if (mid * mid == n){
+        while ( s<=e ) {
+                int mid = ( e+s )/2;
+                if ( mid * mid == n){
                         ans = mid;
                         break;
                 }
                 else if (mid * mid < n){
-                        ans = mid;
                         s = mid+1;
+                        ans = mid;
                 }
-                else{
+                else {
                         e = mid-1;
                 }
         }
-        float decimal = 0.1,mul=1;
-        for(int i=1;i<=p;i++){
-                while(ans * ans <= n){
+        float decimal = 0.1;
+        for ( int i=1;i<=p;i++ ){
+                while ( ans * ans <= n){
                         ans+=decimal;
                 }
-                mul = mul*10;
                 ans-=decimal;
                 decimal/=10;
         }
-        float val = round(ans*mul)/mul;
-    return val;
+        return ans;
 }
-int main ( void ) {
+int main ( ) {
         int n, p;
-        scanf ("%d %d", &n, &p);
-        printf("%g", square_root(n, p));
+        scanf ( "%d %d", &n, &p );
+        printf ( "%.*f\n", p, square_root(n, p) );
         return 0;
 }
