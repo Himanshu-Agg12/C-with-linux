0d30ab979e0ec5a0a3e71ec748c7d6eb52be426c
vagrant <vagrant@myvm18>
2021-08-07 16:09:08 +0000

    Assignment-3

---
+++ stacks_queues/queue.c
@@ -0,0 +1,129 @@
+/*
+ * Program to ...
+ *
+ * @Himanshu Aggarwal  ID -> 1911981022
+ *
+ * Assignment no. -> xx (name) 
+ *
+ *Date
+ *
+ * Question number -> xx
+ */
+#include <stdio.h>
+#define size 5
+
+void enqueue(int[], int);
+void dequeue(int[]);
+void display(int[]);
+int peek(int[]);
+int is_empty();
+
+int front =  - 1;
+int rear =  - 1;
+
+int main()
+{
+    int n, ch;
+    int queue[size];
+    do
+    {
+        printf("\n\n Circular Queue:\n1. Insert \n2. Delete\n3. Display\n4. Peek\n5. is_empty\n0. Exit");
+        printf("\nEnter Choice 0-5? : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                printf("\nEnter number: ");
+                scanf("%d", &n);
+                enqueue(queue, n);
+                break;
+            case 2:
+                dequeue(queue);
+                break;
+            case 3:
+                display(queue);
+                break;
+            case 4:
+                printf("%d\n", peek(queue));
+                break;
+            case 5:
+                printf("%s\n", is_empty() ? "Queue is Empty!" : "Queue is not Empty");
+                break;
+        }
+    }while (ch != 0);
+}
+
+int peek(int queue[])
+{
+    return queue[front];
+}
+
+int is_empty()
+{
+    if(front == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void enqueue(int queue[], int item)
+{
+    if ((front == 0 && rear == size - 1) || (front == rear + 1))
+    {
+        printf("queue is full");
+        return;
+    }
+    else if (rear ==  - 1)
+    {
+        rear++;
+        front++;
+    }
+    else if (rear == size - 1 && front > 0)
+    {
+        rear = 0;
+    }
+    else
+    {
+        rear++;
+    }
+    queue[rear] = item;
+}
+
+void display(int queue[])
+{
+    int i;
+    printf("\n");
+    if (front > rear)
+    {
+        for (i = front; i < size; i++)
+        {
+            printf("%d ", queue[i]);
+        }
+        for (i = 0; i <= rear; i++)
+            printf("%d ", queue[i]);
+    }
+    else
+    {
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue[i]);
+    }
+}
+
+void dequeue(int queue[])
+{
+    if (front ==  - 1)
+    {
+        printf("Queue is empty ");
+    }
+    else if (front == rear)
+    {
+        printf("\n %d deleted", queue[front]);
+        front =  - 1;
+        rear =  - 1;
+    }
+    else
+    {
+        printf("\n %d deleted", queue[front]);
+        front++;
+    }
+}
---
+++ stacks_queues/stack.c
@@ -0,0 +1,112 @@
+/*
+ * Program to implement stack functions.
+ *
+ * @Himanshu Aggarwal  ID -> 1911981022
+ *
+ * Assignment no. -> 04 (Stacks_and_Queues) 
+ *
+ *02/08/2021
+ *
+ * Question number -> 01
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int size = 5;
+int* stack;
+int top = -1;
+
+bool is_empty()
+{
+    if (top == -1)
+        return true;
+    else
+        return false;
+}
+
+void resize_stack(){
+    stack = (int*) realloc(stack, (size*2)*sizeof(int));
+    size = (size * 2);
+}
+
+void push(int value){
+    if(top != size - 1){
+        top = top + 1;
+        *(stack + top) = value;
+    }
+    else{
+        printf("\nResizing the stack to its double.{Overflow}\n");
+        resize_stack();
+    }
+}
+
+void pop() {
+    if (is_empty())
+        printf("\nCan't delete as Stack is already empty.{Underflow}\n");
+    else {
+        int value = *(stack + top);
+        top = top - 1;
+        printf("%d is deleted\n", value);
+    }
+}
+
+int peek() {
+    if (top != -1)
+        return *(stack + top);
+    else
+        return -1;
+}
+
+void print() {
+    int i = top;
+    while(i >= 0){
+        printf("%d ", stack[i]);
+        i--;
+    }
+    printf("\n");
+}
+
+int main()
+{  
+    stack = (int*) calloc(size, sizeof(int));
+    int next = 1;
+
+    while(next){
+        printf("Press 1/2/3/4 for insert/delete/top/empty respectivaly...\n");
+        int choice;
+        scanf("%d", &choice);
+
+        switch(choice) {
+        case 1:
+            printf("Enter a value to insert\n");
+            int value;
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            printf("Most recently used element will be deleted\n");
+            pop();
+            break;
+        case 3:
+            printf("Top most element is: %d \n", peek());
+            break;
+        case 4:
+            printf("Stack is %s \n", is_empty() ? "empty" : "not empty");
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+        } 
+        printf("\nDo you want to continue: 1/0\n");
+        scanf("%d", &next);
+    }
+    
+    print();
+
+    printf("Exit success\n");
+    free(stack);
+
+    return 0;
+}
+
