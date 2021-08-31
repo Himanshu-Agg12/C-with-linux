95adca1be9c16fd6b993b7756b1aef3254510d4e
Himanshu Aggarwal <67819808+Himanshu-Agg12@users.noreply.github.com>
2021-08-31 10:48:28 +0530

    Assignment- 6

--- linked_list/palindrome_linkedList.c
+++ linked_list/palindrome_linkedList.c
@@ -12,24 +12,36 @@
  */
 /**********************************Pseudo Code LeetCode*********************/
 
+//this function will check for the palindrom by recursion
 bool recur(struct ListNode **start, struct ListNode *end){
+    //if there is no element in list
     if(end == NULL)
         return true;
     
+    //it will check for the palindrome or note at any time
     bool isEqual;
+    
+    //it stores the values of the nodes
     int num = end -> val;
     
+    //recursively move the end to the end of list
     isEqual = recur(start, end -> next);
     
+    //if at anytime isEqual false it means not equal so return false.
     if(isEqual == false)
         return false;
+    
+    //these are recursive callbacks so now the num is pointing to the last element and we will compare
+    //with start of the list, also the start should be global or passed by refrence as because changes in start should not lost.
     if((*start) -> val == num){
+        //after comapering start with num we move start towards end and end is coming towards head so when they meet it will palindrome
         *start = (*start) -> next;
         return true;
     }else{
         return false;
     }
 }
+
 bool isPalindrome(struct ListNode* head){
     return recur(&head, head);
 }
