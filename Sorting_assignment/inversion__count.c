abfed70cf02c0a0bf33650adcc58242b0766c8e1
Himanshu Aggarwal <67819808+Himanshu-Agg12@users.noreply.github.com>
2021-09-11 02:21:55 +0530

    Sorting Algo

--- Sorting_assignment/inversion__count.c
+++ Sorting_assignment/inversion__count.c
@@ -2,8 +2,8 @@
 
 int compare(int arr[], int s, int m, int e)
 {
-    int left_size = m - s + 1;
-    int right_size = e - m;
+    int left_size = m - s;
+    int right_size = e - m - 1;
     int left_arr[left_size];
     int right_arr[right_size];
     for(int i = 0; i < left_size; i++){
@@ -38,14 +38,15 @@ int compare(int arr[], int s, int m, int e)
 
 int count_inversion(int arr[], int start, int end)
 {   int count = 0;
-    if(start >= end)
-        return count;
-
+    // if(start >= end)
+    //     return count;
+    if(start < end){
     int mid = (start + end) / 2;
-
     count += count_inversion(arr, start, mid);
     count += count_inversion(arr, mid + 1, end);
     count += compare(arr, start, mid, end);
+    }
+    return count;
 }
 
 int main()
